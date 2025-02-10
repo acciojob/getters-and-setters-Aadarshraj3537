1445ff55527b94860ff92fe31efadce4fe5a836d
Aadarsh Raj <aadarshraj351@gmail.com>
2025-02-10 11:37:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-//complete this code
-class Person {}
+// Base class Person
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-class Student extends Person {}
+  // Method to display basic info
+  getInfo() {
+    return `${this.name} is ${this.age} years old.`;
+  }
+}
 
-class Teacher extends Person {}
+// Student class extending Person
+class Student extends Person {
+  constructor(name, age, grade) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  // Method specific to Student
+  getGrade() {
+    return `${this.name} is in grade ${this.grade}.`;
+  }
+}
+
+// Teacher class extending Person
+class Teacher extends Person {
+  constructor(name, age, subject) {
+    super(name, age);
+    this.subject = subject;
+  }
+
+  // Method specific to Teacher
+  getSubject() {
+    return `${this.name} teaches ${this.subject}.`;
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
